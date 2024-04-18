589eb121b952f5b5dc5eb9d14113f4f7e66da49d
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2024-04-18 08:49:15 +0200

    WIP: Working on loggin endpoint

--- src/api/hello-world.ts
+++ src/api/hello-world.ts
@@ -3,7 +3,13 @@ import middy from '@middy/core';
 export const lambdaHandler = async (event: any = {}): Promise<any> => {
   console.log(event);
 
-  return { statusCode: 201, body: 'こんにちは！' };
+  return {
+    statusCode: 200,
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      message: 'こんにちは！',
+    }),
+  };
 };
 
 const handler = middy(lambdaHandler);
