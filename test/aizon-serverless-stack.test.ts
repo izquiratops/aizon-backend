9e61f9487dfae1270aaf9b3f5494ad212343f495
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2024-04-20 11:40:13 +0200

    Tried to add testing, got a problem because of ES Modules with @middy

--- test/aizon-serverless-stack.test.ts
+++ test/aizon-serverless-stack.test.ts
@@ -1,1 +1,35 @@
-test('SQS Queue Created', () => {});
+const getWidget = require('../src/api/widget/get-widget');
+const AWS = require('aws-sdk');
+
+describe('GET Widget', () => {
+  beforeEach(() => {
+    AWS.mock(
+      'DynamoDB.DocumentClient',
+      'get',
+      (params: any, callback: Function) => {
+        // Simulate DynamoDB response
+        if (params.TableName === 'Widget' && params.Key.widgetId === '0') {
+          callback(null, {
+            Item: {
+              id: '0',
+              name: 'TestObject',
+              type: 'BarChart',
+            },
+          });
+        } else {
+          callback('Invalid parameters', null);
+        }
+      }
+    );
+  });
+
+  test('should return the widget with the specified id', async () => {
+    const result = await getWidget('0');
+
+    expect(result).toEqual({
+      id: '0',
+      name: 'TestObject',
+      type: 'BarChart',
+    });
+  });
+});
