b4749f76bf8704126b31150bbf018e1af8c749b6
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2024-04-21 10:35:43 +0200

    Jest configuration to make testing working fine

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,20 @@
+const esModules = ['@middy'].join('|');
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`],
+  // transformIgnorePatterns: ['/node_modules/(?!(@middy/core)/)'],
+  moduleNameMapper: {
+    '^@middy/core$': '<rootDir>/node_modules/@middy/core',
+  },
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
 };
